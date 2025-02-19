145cc3d39c7ffc66ddcd5c030dff5b0967da27a9
Isaiah Gonzales <isaiahg46@gmail.com>
2025-02-18 20:18:35 -0800

    updated auth.ts file

---
+++ Develop/client/src/utils/auth.ts
@@ -0,0 +1,73 @@
+// use this to decode a token and get the user's information out of it
+import { jwtDecode } from 'jwt-decode';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.sendStatus(401); // Unauthorized
+
+    jwt.verify(token, 'password123', (err: jwt.VerifyErrors | null, user: any) => {
+      if (err) return res.sendStatus(403); // Forbidden
+      (req as any).user = user; // Attach user info to request
+      next();
+    });
+};
+
+
+interface UserToken {
+  name: string;
+  exp: number;
+}
+
+// create a new class to instantiate for a user
+class AuthService {
+  // get user data
+  getProfile() {
+    return jwtDecode(this.getToken() || '');
+  }
+
+  // check if user's logged in
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token); // handwaiving here
+  }
+
+  // check if token is expired
+  isTokenExpired(token: string) {
+    try {
+      const decoded = jwtDecode<UserToken>(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } 
+      
+      return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem('id_token');
+  }
+
+  login(idToken: string) {
+    // Saves user token to localStorage
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem('id_token');
+    // this will reload the page and reset the state of the application
+    window.location.assign('/');
+  }
+}
+
+export default new AuthService();
+export { authenticateToken };
